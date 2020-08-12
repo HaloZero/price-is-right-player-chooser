cad3c8c6ff86175c0b45f79856ea325f2c430aa3
rohan <rohan.dhaimade@gmail.com>
2020-08-11 21:17:07 -0700

    Modified player picker script to run as a command line

--- player_picker.py
+++ player_picker.py
@@ -7,26 +7,43 @@ import random
 import numpy as np
 import pdb
 
-import argparse
+# import argparse
 
-parser = argparse.ArgumentParser(description='Pick out a player')
-parser.add_argument("-p", "--playerschosen", help="Comma delimited list of players already chosen")
+# parser = argparse.ArgumentParser(description='Pick out a player')
+# parser.add_argument("-p", "--playerschosen", help="Comma delimited list of players already chosen")
 
-args = parser.parse_args()
-players_chosen = args.playerschosen
-names_to_remove = players_chosen.split(",") if players_chosen else []
+# args = parser.parse_args()
+# players_chosen = args.playerschosen
+# names_to_remove = players_chosen.split(",") if players_chosen else []
 
-df = pd.read_csv('Names.csv') 
-names = []
+names_to_remove = []
 
-for col in df.columns:
-	new_names = df.get(col).values
-	new_names = new_names[new_names != None]
-	new_names = new_names[~pd.isnull(new_names)]
-	stripped_names = list(map(lambda name: name.strip(), new_names))
-	names = names + list(filter(lambda name: name != "", stripped_names))
+def reloadNames():
+	names = []
+	df = pd.read_csv('Names.csv')
+	for col in df.columns:
+		new_names = df.get(col).values
+		new_names = new_names[new_names != None]
+		new_names = new_names[~pd.isnull(new_names)]
+		stripped_names = list(map(lambda name: name.strip(), new_names))
+		names = names + list(filter(lambda name: name != "", stripped_names))
+	return names
 
-filtered_names = [name for name in names if name not in names_to_remove]
-player_called = random.choice(names)
-print(player_called + ", Come On Down!!!")
+names = reloadNames()
 
+while (True):
+	command = input("Enter command. reload for reloading CSV, choose to pick player\n")
+	if command == "reload":
+		names = reloadNames()
+	elif command == "choose":
+		filtered_names = [name for name in names if name not in names_to_remove]
+		if filtered_names == []:
+			print("No players left to choose")
+		else:
+			player_called = random.choice(filtered_names)
+			print("Come on down " + player_called)
+			names_to_remove.append(player_called)
+	elif command == "exit":
+		exit()
+	else:
+		print("command not recognized")
